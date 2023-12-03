e3f1e075b7444a30878a562b7efe49b10907157a
Arunkumar1102 <arunpanjugula011@gmail.com>
2023-12-03 11:28:51 +0530

    half data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginSignup from './pages/LoginSignup';
+import Shop from './pages/shop';
+import ShopCategory from './pages/shopcategory';
+import Product from './pages/product';
+import Cart from './pages/cart';
 
 function App() {
   return (
     <div >
-     <Navbar />
+      
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<ShopCategory category="men"/>} />
+          <Route path='/womens' element={<ShopCategory category="women"/>} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/product' element={<Product/>} />
+            <Route path='/productId' element={<Product />} />
+          <Route />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/Login' element={<LoginSignup/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
